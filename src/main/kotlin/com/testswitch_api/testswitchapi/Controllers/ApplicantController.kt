5ab8ec054d5d4581f5c0fdc05e62ea765bac5366
Jed Johnson <jed.johnson@softwire.com>
2020-02-06 14:31:34 +0000

    First attempt at controller

--- src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
+++ src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
@@ -1,14 +1,19 @@
 package com.testswitch_api.testswitchapi.Controllers
 
 import com.testswitch_api.testswitchapi.Models.Application
+import com.testswitch_api.testswitchapi.Models.DatabaseApplication
+import com.testswitch_api.testswitchapi.Services.ApplicationService
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ApplicantController {
+
     @PostMapping("/application")
-    fun storeApplication(@ModelAttribute application: Application) :Application{
+    fun storeApplication(@ModelAttribute application: Application) : DatabaseApplication {
+        var applicationService = ApplicationService
+        var databaseApplication : DatabaseApplication = applicationService.addApplicant(application)
         return application
     }
 }
