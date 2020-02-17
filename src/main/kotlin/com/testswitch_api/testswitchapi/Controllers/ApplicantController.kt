1bd2c748a4adda9740fdbc33f925368b0aa9e39f
Jed Johnson <jed.johnson@softwire.com>
2020-02-17 14:50:16 +0000

    Created get all applicants endpoint

--- src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
+++ src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
@@ -4,7 +4,9 @@ import com.testswitch_api.testswitchapi.Models.Application
 import com.testswitch_api.testswitchapi.Models.DatabaseApplication
 import com.testswitch_api.testswitchapi.Services.ApplicationService
 import org.jdbi.v3.core.Jdbi
+import org.omg.CORBA.DATA_CONVERSION
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -15,9 +17,14 @@ class ApplicantController @Autowired constructor(
 ){
 
     @PostMapping("/add")
-    fun storeApplication(@ModelAttribute application: Application) : DatabaseApplication {
-//        var applicationService : ApplicationService
-        var databaseApplication : DatabaseApplication = applicationService.addApplicant(application)
-        return databaseApplication
+    fun storeApplication(@ModelAttribute application: Application) : ResponseEntity<Any> {
+        applicationService.addApplicant(application)
+        return ResponseEntity.ok().build()
     }
+
+    @GetMapping("/get_all")
+    fun getAllApplications() : List<DatabaseApplication>{
+        return applicationService.getAllApplicants()
+    }
+
 }
