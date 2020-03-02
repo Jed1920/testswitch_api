ace03baf3e425d8f4337a61d4dfb784764361df5
Jed Johnson <jed.johnson@softwire.com>
2020-03-02 19:14:02 +0000

    file name in s3 made from applicant fields

--- src/main/kotlin/com/testswitch_api/testswitchapi/Services/UploadObject.kt
+++ src/main/kotlin/com/testswitch_api/testswitchapi/Services/UploadObject.kt
@@ -2,23 +2,20 @@ package com.testswitch_api.testswitchapi.Services
 
 import com.amazonaws.AmazonServiceException
 import com.amazonaws.SdkClientException
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
+import com.testswitch_api.testswitchapi.Models.DatabaseApplication
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.IOException
 
 @Service
 class UploadObject constructor (var s3Client : AmazonS3) {
     @Throws(IOException::class)
-    fun uploadFile(file : File) {
+    fun uploadFile(name : String,id : Int,file : File) {
         val bucketName = System.getenv("AWS_BUCKET_NAME")
-        val fileObjKeyName = "FileTrial04"
-        val fileName = "C:/Users/jedjo/OneDrive/Documents/CV/Johnson_Jed_CV.pdf"
+        val fileObjKeyName = "${id}_${name.replace(" ","_")}"
         try {
             val request = PutObjectRequest(bucketName, fileObjKeyName, file)
             val metadata = ObjectMetadata()
