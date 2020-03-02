757d9c7ac4d5f15427bffdd5c97988ecd8bc8cb3
Jed Johnson <jed.johnson@softwire.com>
2020-03-02 19:13:46 +0000

    Included cv availability in database

--- src/main/kotlin/com/testswitch_api/testswitchapi/Models/DatabaseModels.kt
+++ src/main/kotlin/com/testswitch_api/testswitchapi/Models/DatabaseModels.kt
@@ -1,6 +1,6 @@
 package com.testswitch_api.testswitchapi.Models
 
-data class DatabaseApplication(val id:Int, val name: String, val email: String, val contactInfo: String, val experience: ExperienceLevel,val applicationState: ApplicationState)
+data class DatabaseApplication(val id:Int, val name: String, val email: String, val contactInfo: String, val experience: ExperienceLevel,val applicationState: ApplicationState,val cv : Boolean)
 
 enum class ApplicationState() {
     NEW,
