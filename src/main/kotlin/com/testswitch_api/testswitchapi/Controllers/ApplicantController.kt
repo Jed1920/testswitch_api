f2efb7561d7d42910cdeeb9ab8c56d6251021279
Jed Johnson <jed.johnson@softwire.com>
2020-03-02 19:15:24 +0000

    Controller refactored
    upload added to add applicant endpoint
    generateUrl endpoint to retrieve url of cv location

--- src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
+++ src/main/kotlin/com/testswitch_api/testswitchapi/Controllers/ApplicantController.kt
@@ -10,79 +10,56 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
 
 @RestController
 @RequestMapping("/application")
 @CrossOrigin("http://localhost:3000")
 class ApplicantController @Autowired constructor(
-        private val applicationService : ApplicationService,
-        private val uploadFile : UploadObject,
-        private val generateUrl : GenerateURL
-){
-
+        private val applicationService: ApplicationService,
+        private val uploadFile: UploadObject,
+        private val generateUrl: GenerateURL
+) {
     @PostMapping("/add")
-    fun storeApplication(@ModelAttribute application: Application) : ResponseEntity<Any> {
-        applicationService.addApplicant(application)
-        return ResponseEntity.ok().build()
+    fun storeApplication(@ModelAttribute application: Application, @ModelAttribute file: MultipartFile?): ResponseEntity<Any> {
+        try {
+            applicationService.addApplicant(application, file)
+            return ResponseEntity.ok().build()
+        } catch (e: Exception) {
+            return ResponseEntity.badRequest().build()
+        }
     }
 
     @GetMapping("/get_all")
-    fun getAllApplications() : List<DatabaseApplication>{
+    fun getAllApplications(): List<DatabaseApplication> {
         return applicationService.getAllApplicants()
     }
 
     @GetMapping("/get_applicant/{id}")
-    fun getAllApplications(@PathVariable id: Int) : DatabaseApplication{
+    fun getAllApplications(@PathVariable id: Int): DatabaseApplication {
         return applicationService.getApplicantById(id)
     }
 
     @GetMapping("/change_state/{id}/{state}")
-    fun updateApplicationstate(@PathVariable id:Int, @PathVariable state: ApplicationState) : DatabaseApplication{
-        applicationService.updateApplicationState(id,state)
+    fun updateApplicationstate(@PathVariable id: Int, @PathVariable state: ApplicationState): DatabaseApplication {
+        applicationService.updateApplicationState(id, state)
         return applicationService.getApplicantById(id)
     }
 
     @GetMapping("/test/{testString}")
-    fun getApplicantTest(@PathVariable testString:String) : ResponseEntity<Any>{
+    fun getApplicantTest(@PathVariable testString: String): ResponseEntity<Any> {
         try {
             var applicationId = applicationService.getApplicationIdByTestString(testString)
             return ResponseEntity.ok().body(applicationService.getApplicantById(applicationId))
-        } catch(e: Exception) {
+        } catch (e: Exception) {
             return ResponseEntity.notFound().build()
         }
     }
+    
+    @GetMapping("get_url/{objectKey}")
+    fun generateUrl(@PathVariable objectKey: String): ResponseEntity<Any> {
+        data class urlObject(var url: String)
 
-    @PostMapping("fileUpload")
-    fun uploadFiletoS3(@ModelAttribute multiFile: MultipartFile){
-        var file = convert(multiFile)
-        uploadFile.uploadFile(file!!)
-        file.delete()
-    }
-    @GetMapping("getUrl")
-    fun generateUrl() : String{
-        return generateUrl.generateUrl()
-    }
-//    @PostMapping("/uploadFile")
-//    fun uploadFile(@RequestParam("file") file: MultipartFile): UploadFileResponse? {
-//        val fileName: String = fileStorageService?.storeFile(file) ?:"Something went wrong"
-//        val fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/downloadFile/")
-//                .path(fileName)
-//                .toUriString()
-//        return file.contentType?.let { UploadFileResponse(fileName, fileDownloadUri, it, file.size) }
-//    }
-
-    @Throws(IOException::class)
-    fun convert(file: MultipartFile): File? {
-        val convFile = File(file.originalFilename)
-        convFile.createNewFile()
-        val fos = FileOutputStream(convFile)
-        fos.write(file.bytes)
-        fos.close()
-        return convFile
+        return ResponseEntity.ok().body(urlObject(generateUrl.generateUrl(objectKey)))
     }
 }
