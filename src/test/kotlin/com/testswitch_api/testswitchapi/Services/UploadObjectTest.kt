55cd8a86e97bd4c07a1f88000741f5571a2e868b
Jed Johnson <jed.johnson@softwire.com>
2020-03-03 17:30:19 +0000

    Added testing for exceptions thrown

--- src/test/kotlin/com/testswitch_api/testswitchapi/Services/UploadObjectTest.kt
+++ src/test/kotlin/com/testswitch_api/testswitchapi/Services/UploadObjectTest.kt
@@ -1,5 +1,7 @@
 package com.testswitch_api.testswitchapi.Services
 
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.SdkClientException
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.PutObjectRequest
 import org.junit.jupiter.api.Test
@@ -10,7 +12,7 @@ import java.io.File
 internal class UploadObjectTest{
 
     @Test
-    fun uploadObjectTest(){
+    fun successfulUploadObjectTest(){
         val s3Client = Mockito.mock(AmazonS3::class.java)
 
         var uploadObject = UploadObject(s3Client)
@@ -23,4 +25,33 @@ internal class UploadObjectTest{
         Mockito.verify(s3Client, Mockito.atLeastOnce()).putObject(any(PutObjectRequest::class.java))
     }
 
+    @Test
+    fun amazonServiceExceptionUploadObjectTest(){
+        val s3Client = Mockito.mock(AmazonS3::class.java)
+        Mockito.`when`(s3Client.putObject(any(PutObjectRequest::class.java))).thenThrow(AmazonServiceException(""))
+
+        var uploadObject = UploadObject(s3Client)
+        val file = File("")
+        val name = "name"
+        val id = 1
+
+        uploadObject.uploadFile(name,id,file)
+
+        Mockito.verify(s3Client, Mockito.atLeastOnce()).putObject(any(PutObjectRequest::class.java))
+    }
+
+    @Test
+    fun sdkClientExceptionUploadObjectTest(){
+        val s3Client = Mockito.mock(AmazonS3::class.java)
+        Mockito.`when`(s3Client.putObject(any(PutObjectRequest::class.java))).thenThrow(SdkClientException(""))
+
+        var uploadObject = UploadObject(s3Client)
+        val file = File("")
+        val name = "name"
+        val id = 1
+
+        uploadObject.uploadFile(name,id,file)
+
+        Mockito.verify(s3Client, Mockito.atLeastOnce()).putObject(any(PutObjectRequest::class.java))
+    }
 }
